c2e738b27a9448989d4d848b6ce59a11abbeb500
masudalimran <masudalimran92@gmail.com>
2021-11-07 13:54:18 +0600

    finsihed

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,5 @@
 let path = require('path');
+const demo1 = require('../models/demo1')
 let options = {
     root: path.join(__dirname, '../public/posts')
 }
@@ -14,4 +15,77 @@ const posts_c= (req, res) => {
     })
 }
 
-module.exports = {posts_c}
+const allPosts_c = async (req, res)=> {
+    try{
+        const demoPosts = await demo1.find();
+        res.status(200).json(demoPosts)
+    }catch{
+        res.status(400).json({message: err})
+    }
+}
+
+const submitPost_c = async (req,res)=>{
+    // console.log(req.body);
+    const demoPost = new demo1({
+        title : req.body.title,
+        description: req.body.description,
+        price : req.body.price
+    })
+    // demoPost.save()
+    //     .then(data => {
+    //         res.status(200).json(data);
+    //     })
+    //     .catch(err => {
+    //         res.json({message: err})
+    //     })
+    try{
+        const savedDemoPost = await demoPost.save()
+        res.status(200).json(savedDemoPost)
+    }catch(err){
+        res.status(400).json({message: err})
+    }
+}
+
+const specificPost_c = async (req, res)=> {
+    // console.log(req.params.postId);
+    // res.send(`Specific Post ${req.params.postId}`)
+    try{
+        const specificPost = await demo1.findById(req.params.postId)
+        res.json(specificPost)
+    }catch(err){
+        res.json({message: err})
+    }
+}
+
+const deleteSpecificPost_c = async (req, res)=> {
+    try{
+        await demo1.deleteOne({_id: req.params.postId})
+        const allPosts = await demo1.find() 
+        res.json(allPosts)
+    }catch{
+        res.send("Error Found While Deleting Post")
+    }
+}
+
+const updateSpecificPost_c = async (req, res)=>{
+    try{
+        await demo1.updateOne(
+            {_id: req.params.postId},
+            {$set: 
+                {title: req.body.title, description: req.body.description, price: req.body.price}
+            }
+        )
+        const allPosts = await demo1.find()
+        res.json(allPosts)
+    }catch{
+        res.send("Error Found While Updating Post")
+    }
+}
+module.exports = {
+    posts_c,
+    allPosts_c,
+    submitPost_c,
+    specificPost_c,
+    deleteSpecificPost_c,
+    updateSpecificPost_c
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,82 +1,31 @@
 const express = require('express')
 const posts = express.Router()
-const demo1 = require('../models/demo1')
+// const demo1 = require('../models/demo1')
 
-const {posts_c} = require('../controllers/postsController')
+const {
+    posts_c,
+    allPosts_c,
+    submitPost_c,
+    specificPost_c,
+    deleteSpecificPost_c,
+    updateSpecificPost_c
+} = require('../controllers/postsController')
 // const { post } = require('./home')
 
 posts.get('/', posts_c)
 
 // Get all posts
-posts.get('/all', async (req, res)=> {
-    try{
-        const demoPosts = await demo1.find();
-        res.status(200).json(demoPosts)
-    }catch{
-        res.status(400).json({message: err})
-    }
-})
-
+posts.get('/all', allPosts_c)
 
 // Submits a post
-posts.post('/', async (req,res)=>{
-    // console.log(req.body);
-    const demoPost = new demo1({
-        title : req.body.title,
-        description: req.body.description,
-        price : req.body.price
-    })
-    // demoPost.save()
-    //     .then(data => {
-    //         res.status(200).json(data);
-    //     })
-    //     .catch(err => {
-    //         res.json({message: err})
-    //     })
-    try{
-        const savedDemoPost = await demoPost.save()
-        res.status(200).json(savedDemoPost)
-    }catch(err){
-        res.status(400).json({message: err})
-    }
-})
+posts.post('/', submitPost_c)
 
 // Specific Post
-posts.get('/:postId', async (req, res)=> {
-    // console.log(req.params.postId);
-    // res.send(`Specific Post ${req.params.postId}`)
-    try{
-        const specificPost = await demo1.findById(req.params.postId)
-        res.json(specificPost)
-    }catch(err){
-        res.json({message: err})
-    }
-})
+posts.get('/:postId', specificPost_c)
 
 // Delete Specific Post
-posts.delete('/:postId', async (req, res)=> {
-    try{
-        await demo1.deleteOne({_id: req.params.postId})
-        const allPosts = await demo1.find() 
-        res.json(allPosts)
-    }catch{
-        res.send("Error Found While Deleting Post")
-    }
-})
+posts.delete('/:postId', deleteSpecificPost_c)
 
 //  Update a post
-posts.patch('/:postId', async (req, res)=>{
-    try{
-        await demo1.updateOne(
-            {_id: req.params.postId},
-            {$set: 
-                {title: req.body.title, description: req.body.description, price: req.body.price}
-            }
-        )
-        const allPosts = await demo1.find()
-        res.json(allPosts)
-    }catch{
-        res.send("Error Found While Updating Post")
-    }
-})
+posts.patch('/:postId', updateSpecificPost_c)
 module.exports = posts
